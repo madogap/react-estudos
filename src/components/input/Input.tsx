1d5015a4805528904bcd80f83f760470eaf73c93
madogap <madogap@gmail.com>
2023-06-30 14:06:49 -0300

    Destructuring of props and method determined directly by the component

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -2,17 +2,16 @@ import React from "react"
 
 type InputProps = {
     value: string
-    handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+    // handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export const Input = ({value, handleChange}: InputProps) => {
+export const Input = ({value}: InputProps) => {
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         console.log(event)
     }
     return (
-        <input type="text" value={value} onChange={handleChange} />
+        <input type="text" value={value} onChange={handleInputChange}/>
     )
 }
 //handleChange:(event:React.ChangeEvent<HTMLInputElement>)=> void
 //<input type="text" value={props.value} onChange={props.handleChange}/>
-//
