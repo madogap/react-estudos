d377b1e9cd240564f05556a19ea6f39ae53ec6d1
madogap <madogap@gmail.com>
2023-06-30 14:03:30 -0300

    Destructuring of props

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -5,8 +5,8 @@ type ButtonProps = {
     handleClick: (event: React.MouseEvent<HTMLButtonElement>, id: number) => void
 }
 
-export const Button = (props: ButtonProps) => {
+export const Button = ({handleClick}: ButtonProps) => {
     return (
-        <button onClick={(event) => props.handleClick(event, 1)}>click</button>
+        <button onClick={(event) => handleClick(event, 1)}>click</button>
     )
 }
