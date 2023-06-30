094a36acf1c451adb838085f3dba0b1fcc15b28e
madogap <madogap@gmail.com>
2023-06-30 14:10:01 -0300

    Implementation of age properties with no obligation of value and which can be undefined

--- src/components/person/Person.Types.tsx
+++ src/components/person/Person.Types.tsx
@@ -1,8 +1,19 @@
 export type Name = {
     first: string
     last: string
+    age?: number | undefined
+
 }
 
 export type PersonProps = {
     name: Name
+    age?: number | undefined
+    
+}
+
+
+export type Endereco = {
+    cidade: string
+    bairro: string
+    cep: number
 }
