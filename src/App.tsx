f9e96245be7a8dde39508030b595875c6d7ab28d
madogap <madogap@gmail.com>
2023-06-30 13:59:39 -0300

    Add tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,107 @@
 import './App.css'
-import { Title } from './components/title/Title'
-import { Person } from './components/person/Person'
-import { PersonList } from './components/personList/PersonList'
-import { Status } from './components/status/Status'
-import { Heading } from './components/heading/Heading'
-import { Oscar } from './components/oscar/Oscar'
+// import { Title } from './components/title/Title'
+// import { Person } from './components/person/Person'
+// import { PersonList } from './components/personList/PersonList'
+// import { Status } from './components/status/Status'
+// import { Heading } from './components/heading/Heading'
+// import { Oscar } from './components/oscar/Oscar'
 import { Button } from './components/button/Button'
 import { Input } from './components/input/Input'
 
 import { LoggedIn } from './components/state/LoggedIn'
-
-
+import { User } from './components/state/User'
 
 
 function App() {
-  const personName = {
-    first: 'Bruce',
-    last: 'Wayne',
-  }
-  const nameList = [
-    {
-      first: 'Bruce',
-      last: 'Wayne',
-    },
-    {
-      first: 'Clark',
-      last: 'Ken',
-    },
-    {
-      first: 'Aquiles',
-      last: 'We',
-    },
-    {
-      first: 'Test3',
-      last: 'W',
-    },
-    {
-      first: 'Jiuiu',
-      last: 'Wayne',
-    },
-    {
-      first: 'jajssjasas',
-      last: 'sadasdv',
-    },
-  ]
+  // const personName = {
+  //   first: 'Marcus',
+  //   last: 'Augusto',
 
+  // }
+
+  // const nameList = [
+  //   {
+  //     first: 'Bruce',
+  //     last: 'Wayne',
+  //     age: 88
+  //   },
+  //   {
+  //     first: 'Clark',
+  //     last: 'Ken',
+
+  //   },
+  //   {
+  //     first: 'Aquiles',
+  //     last: 'We',
+  //     age: 100
+  //   },
+  //   {
+  //     first: 'Test3',
+  //     last: 'W',
+  //     age: 0
+  //   },
+  //   {
+  //     first: 'Jiuiu',
+  //     last: 'Wayne',
+  //     age: 200
+  //   },
+  //   {
+  //     first: 'jajssjasas',
+  //     last: 'sadasdv',
+  //     age: 1999
+  //   },
+  // ]
+
+  // return (
+  //   <>
+  //     <div>
+  //       <Title name="Welcome Vite" messageCount={90} isLoggeIn={false} />
+  //       <Title name="Welcome Vite" isLoggeIn={true} />
+  //     </div>
+  //     <hr />
+
+  //     <Person name={personName} />
+  //     <hr />
+  //     <PersonList names={nameList} />
+  //     <hr />
+
+  //     <Status status='loading' />
+  //     <hr />
+  //     <Status status='sucess' />
+  //     <hr />
+  //     <Status status='error' />
+  //     <hr />
+  //     <div className='App' >
+  //       <Status status='loading' />
+  //       <Heading>Este component permite digitar entre o Heading Component</Heading>
+  //       <Oscar>
+  //         <Heading>O Componente Oscar permite criar um Filho como componete entre suas tag de componente</Heading>
+  //       </Oscar>
+  //     </div>
+
+  
   return (
     <>
-      <Title name="Welcome Vite" messageCount={35} isLoggeIn={false} />
-      <hr />
-      <Person name={personName} />
-      <hr />
-      <PersonList names={nameList} />
-      <hr />
-      <Status status='loading' />
-      <hr />
-      <Status status='sucess' />
-      <hr />
-      <Status status='error' />
-      <hr />
+
+
       <div className='App'>
-        <Status status='loading' />
-        <Heading>Placeholder tex</Heading>
-        <Oscar>
-          <Heading>Oscar goes to Leonardo Dicpario</Heading>
-        </Oscar>
+        <Button handleClick={
+          (event, id) => {
+            console.log
+              ('Button clicked', event, id)
+
+          }} />
       </div>
-      <Title name="Welcome Vite" isLoggeIn={false} />
-      <div className='App'>
-        <Button handleClick={(event, id) => {
-          console.log('Button clicked', event, id)
-        }} />
-        <hr />
-        <Input value='' handleChange={(event) => console.log(event)} />
+      <br />
+      <div>
+        <Input value='' handleChange={
+          (event) => console.log(event)} />
       </div>
-      
-      
-      <LoggedIn/>
+      <br />
+
+      <LoggedIn />
+      <User/>
+
     </>
   )
 }
